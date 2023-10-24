cae68f3e69a4c2b756506f9186c7700c9aeb24d5
Dannidev <dante.rodriguez.c@tecsup.edu.pe>
2023-10-24 16:57:29 -0500

    Initial Commit

---
+++ ColeccionDeJuegps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegps
+//
+//  Created by Dante Rodriguez on 24/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
